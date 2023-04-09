24974d4f7e65e88b942d24001206eae57a874484
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-08 23:06:14 -0600

    Add comments to code Explain how we get the computer choice and define winner of round

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getComputerChoice() {
+    // Use Math.random * 3 to get a number between 0 and 2 to define the computer's choice.
     let hand = Math.floor(Math.random() * 3);
     switch (hand) {
         case 0:
@@ -21,11 +22,13 @@ function getPlayerChoice() {
 }
 
 function playRound() {
+    // Run funcitions to get both the player and computer choices
     let player = getPlayerChoice();
     player = player.toLowerCase();
     let computer = getComputerChoice();
     console.log(player);
     console.log(computer);
+    // Run validation for tie, if not then all scenarios where player wins and else player loses.
     if (player == computer) {
         return `Tie! Both chose ${player}`;
     }
