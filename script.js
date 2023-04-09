e779b77d799e71f958cbc1d06a4a4181bd172b36
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-08 22:48:27 -0600

    Create function to play one match of RPS.

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function getComputerChoice() {
     let hand = Math.floor(Math.random() * 3);
     switch (hand) {
         case 0:
-            return 'Rock';
+            return 'rock';
             break;
         case 1:
-            return 'Paper';
+            return 'paper';
             break;
         case 2:
-            return 'Scissors';
+            return 'scissors';
             break;
         default:
             return 'This was not supposed to happen';
@@ -16,4 +16,24 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(player, computer) {
+    player = player.toLowerCase();
+    console.log(player);
+    if (player == computer) {
+        return `Tie! Both chose ${player}`;
+    }
+    else if (player == 'rock' && computer == 'scissors' || player == 'paper' && computer == 'rock' || player == 'scissors' && computer == 'paper') {
+        return `You win! ${player} beats ${computer}`;
+    }
+    else {
+        return `You lose! ${computer} beats ${player}`;
+    }
+
+}
+
+
+const playerSelection = 'Rock';
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
