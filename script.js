4b4c4102ea0b422e408f01dda3c638c45c25119f
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-13 15:57:55 -0600

    Simplify playRound function Create a new determineRoundWinner function to improve readability

--- script.js
+++ script.js
@@ -24,28 +24,27 @@ function playRound() {
     // Run funcitions to get computer choices and get value of button pressed
     let player = this.value
     let computer = getComputerChoice();
-    let resultRound = document.querySelector('#resultRound');
-    console.log(player);
-    console.log(computer);
+    determineRoundWinner(player, computer);
+    updateScores();
+}
+
+function determineRoundWinner(player, computer) {
+    const resultRound = document.querySelector('#resultRound');
     // Run validation for tie, if not then all scenarios where player wins and else player loses.
     if (player == computer) {
         console.log(`Tie! Both chose ${player}`);
         resultRound.textContent = `Tie! Both chose ${player}`;
-        //return 0;
     }
     else if (player == 'Rock' && computer == 'Scissors' || player == 'Paper' && computer == 'Rock' || player == 'Scissors' && computer == 'Paper') {
         console.log(`You win! ${player} beats ${computer}`);
         resultRound.textContent = `You win! ${player} beats ${computer}`;
         playerScore++;
-        //return 1;
     }
     else {
         console.log(`You lose! ${player} is beaten by ${computer}`);
         resultRound.textContent = `You lose! ${player} is beaten by ${computer}`;
         computerScore++;
-        //return 2;
     }
-    updateScores();
 }
 
 function updateScores() {
