888bf83ad706171027a1d1eb0897baf2d74b924d
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-13 13:07:46 -0600

    Add div to display the results of round

--- script.js
+++ script.js
@@ -26,19 +26,23 @@ function playRound() {
     let player = this.value
     player = player.toLowerCase();
     let computer = getComputerChoice();
+    let resultRound = document.querySelector('#resultRound');
     console.log(player);
     console.log(computer);
     // Run validation for tie, if not then all scenarios where player wins and else player loses.
     if (player == computer) {
         console.log(`Tie! Both chose ${player}`);
+        resultRound.textContent = `Tie! Both chose ${player}`;
         return 0;
     }
     else if (player == 'rock' && computer == 'scissors' || player == 'paper' && computer == 'rock' || player == 'scissors' && computer == 'paper') {
         console.log(`You win! ${player} beats ${computer}`);
+        resultRound.textContent = `You win! ${player} beats ${computer}`;
         return 1;
     }
     else {
-        console.log(`You lose! ${computer} beats ${player}`);
+        console.log(`You lose! ${player} is beaten by ${computer}`);
+        resultRound.textContent = `You lose! ${player} is beaten by ${computer}`;
         return 2;
     }
 
